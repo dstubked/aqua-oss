30bea90363c70e0357d22023ae006e8bd4597421
dstubked <45514912+dstubked@users.noreply.github.com>
2020-03-28 00:19:39 +0800

    Update bootstrap_kworker.sh

--- bootstrap_kworker.sh
+++ bootstrap_kworker.sh
@@ -6,3 +6,6 @@ apt-get  install -y sshpass >/dev/null 2>&1
 #sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /joincluster.sh 2>/dev/null
 sshpass -p "kubeadmin" scp -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /joincluster.sh
 bash /joincluster.sh >/dev/null 2>&1
+# Copy kubecfg file into worker
+mkdir /home/vagrant/.kube
+sshpass -p "kubeadmin" scp -o StrictHostKeyChecking=no kmaster.example.com:/home/vagrant/.kube/config /home/vagrant/.kube/config
